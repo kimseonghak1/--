59998340d254a17b231212176c86e7b4758c6f00
DESKTOP-4TVSQF2\KSH <jip9821@cu.ac.kr>
2022-06-23 10:28:34 +0900

    first commit

---
+++ abcd/index.js
@@ -0,0 +1,46 @@
+const { use } = require("express/lib/application");
+const WebSocket = require("ws");
+const ws = new WebSocket.Server({port:8008});
+
+let user_id=0; //클라이언트에게 부여되는 고유한 값
+//전체 유저들을 통제할 수 있도록 각 유저에 대한 websocket, user_id 저장
+let ALL_WS = [];
+ws.on("connection", function connect(websocket,req){
+    user_id++;
+    console.log("NEW USER CONNECT("+user_id+")");
+    ALL_WS.push({"ws":websocket,"user_id":user_id, "user_name":""});
+    //~.push() 배열에 추가 요소를 넣는 것이다. 그래서 넣을때, 옵션을 2가지 선택
+    //하나는 ws 요소, 하나는 user_id 요소
+
+    sendUserId(user_id);
+    websocket.on("message", function incoming(message){
+        console.log(JSON.parse(message));
+        message = JSON.parse(message);
+        switch(message.code) {
+            case "connect_name" : // 사용자 추가
+                ALL_WS.forEach(function(element, index){
+                    if(element.user_id==message.user_id){
+                        element.user_name=message.name;
+                    }            
+                });
+                console.log(ALL_WS);
+            break;
+        }
+    });
+    //문자열을 바꿔서 소켓을 통해 서버로 보낸다.
+    function sendUserId(user_id){ //sendUserId 역할
+        let data={"code":"my_user_id","msg":user_id};
+        websocket.send(JSON.stringify(data));
+    } // JSON.stringify는 배열 형태의 이 변수를 문자열로 바꾸어줌.
+
+    function sendAllUsers() { // 전체 사용자 정보를 보냄
+        let data ={"code":"all_users","msg":JSON.stringify(ALL_WS)};
+
+        ALL_WS.forEach(function(element, index){
+            element.ws.send(JSON.stringify(data));
+            if(element.user_id==message.user_id){
+                element.user_name=message.name;
+            }            
+        });
+    }
+});
