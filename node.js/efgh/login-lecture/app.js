2f0ba396d0bb835fcf12e2a79c55183fdadf742d
DESKTOP-4TVSQF2\KSH <jip9821@cu.ac.kr>
2022-06-29 10:54:06 +0900

    폴더구조화

--- node.js/efgh/login-lecture/app.js
+++ node.js/efgh/login-lecture/app.js
@@ -6,11 +6,12 @@ const app = express();
 
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views","./views"); //화면 views가 저장될 폴더 이름은 ./views라고 함.
+app.set("views","./src/views"); //화면 views가 저장될 폴더 이름은 ./views라고 함.
 app.set("view engine","ejs");
+app.use(express.static(`${__dirname}/public`));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
